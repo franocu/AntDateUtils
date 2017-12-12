0114ee9ac043a37a985711394f227244f64d9fb9
Fran <franciscofc@ocu.es>
2017-12-12 10:41:38 +0100

    prueba mensaje

--- AntDateUtils/src/com/mkyong/core/utils/DateUtils.java
+++ AntDateUtils/src/com/mkyong/core/utils/DateUtils.java
@@ -3,7 +3,7 @@ package com.mkyong.core.utils;
 import java.util.Date;
 
 public class DateUtils {
-
+// prueba ffc
 	public static void main(String[] args) {
 
 		System.out.println(getLocalCurrentDate());
